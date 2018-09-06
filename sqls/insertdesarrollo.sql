6c3cdba5bc2cb4720bbc6081709885897cd9004c
yahir <edson.ruvalcaba@alumnos.udg.mx>
2018-09-06 00:59:56 -0500

    insert datos desarrollo

---
+++ sqls/insertdesarrollo.sql
@@ -0,0 +1,26 @@
+INSERT into area(nombre,idDepto)
+VALUES ('desarrollo humano','1'),
+('ti','1'),
+('planeación estrategica','1'),
+('pmo','1')
+
+INSERT into subarea(nombre,idArea) VALUES
+('atracción e inserción del talento','1'),
+('desarrollo organizacional','1'),
+('seguridad patrimonial','1'),
+('relaciones laborales','1'),
+('servicios al personal','1'),
+('compensaciones','1')
+
+INSERT into subarea(nombre,idArea) VALUES
+('desarrollo de información','2'),
+('infraestructura','2'),
+('servicios y soporte ti','2')
+
+INSERT into subarea(nombre,idArea) VALUES
+('costos','3'),
+('presupuestos','3')
+
+INSERT into subarea(nombre,idArea) VALUES
+('oficina de proyectos','4'),
+('administración de proyectos','4')
