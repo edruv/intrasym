1f113a128042206ceb5ba45e8c3658094a3ea7a6
yahir <edson.ruvalcaba@alumnos.udg.mx>
2018-09-06 22:01:46 -0500

    dinamic form ajax php

--- php/areas.php
+++ php/areas.php
@@ -1,2 +1,32 @@
 <?php
+	require_once 'config.php';
+	require_once 'functions.php';
+
+	if (isset($_GET['depto'])) {
+		// $data = dbhome();
+		$data = dbjob();
+
+		$con = conexion($data);
+
+		$stmt = $con->prepare('SELECT * from departamento');
+		$stmt->execute();
+		$deptos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		echo json_encode($deptos);
+	}
+
+	if (isset($_GET['area'])) {
+		$id = $_GET['area'];
+
+		// $data = dbhome();
+		$data = dbjob();
+
+		$con = conexion($data);
+
+		$stmt = $con->prepare('SELECT * from area where idDepto=:id');
+		$stmt->execute(array(':id' => $id));
+
+		$areas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		echo json_encode($areas);
+	}
+
 ?>
