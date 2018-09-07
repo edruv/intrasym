bcd7fffd4360c8f9f42c296e57b777bfead4bc4f
yahir <edson.ruvalcaba@alumnos.udg.mx>
2018-09-06 22:01:25 -0500

    add config and fuctins files conections

--- php/config.php
+++ php/config.php
@@ -1,2 +1,20 @@
 <?php
+	function dbhome(){
+		$dbd = array(
+			'ht' => 'localhost',
+			'db' => 'intrasym',
+			'user' => 'root',
+			'pass' => 'kilo'
+		);
+		return $dbd;
+	}
+	function dbjob(){
+		$dbd = array(
+			'ht' => 'localhost',
+			'db' => 'intrasym',
+			'user' => 'root',
+			'pass' => ''
+		);
+		return $dbd;
+	}
 ?>
--- php/functions.php
+++ php/functions.php
@@ -1,2 +1,10 @@
 <?php
+	function conexion($data){
+		try {
+			$conexion = new PDO('mysql:host='.$data['ht'].';dbname='.$data['db'],$data['user'],$data['pass'],array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+			return $conexion;
+		} catch (PDOException $e) {
+			echo "Error: ". $e->getMessage();
+		}
+	}
 ?>
